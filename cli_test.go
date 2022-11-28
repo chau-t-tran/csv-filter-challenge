fd3446af4377a45a32b013499d0f831fe22b9c78
chau-t-tran <chau.t.tran.us@gmail.com>
2022-11-28 01:04:32 -0500

    Implicit mode CLI test

--- cli_test.go
+++ cli_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"testing"
 
@@ -24,9 +25,34 @@ func (suite *CLITestSuite) SetupSuite() {
 func (suite *CLITestSuite) TestImplicitArgs() {
 	args := []string{"cmd", "-f=data.csv", "-i", "Ken", "Thompson", "19430204"}
 	os.Args = args
-	cli := NewCLI()
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	cli, err := NewCLI()
+	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), "data.csv", cli.path)
 	assert.Equal(suite.T(), IMPLICIT, cli.mode)
+	assert.Equal(suite.T(), suite.orderedArgs, cli.args)
+}
+
+func (suite *CLITestSuite) TestExplicitArgs() {
+	/*
+		Explicit is similar to implicit, except the CLI constructor
+		re-arranges all filter args in the order shown in the .csv
+		file.
+	*/
+	args := []string{
+		"cmd",
+		"-f=data.csv",
+		"-e",
+		"last_name=Thompson",
+		"dob=19430204",
+		"first_name=Ken",
+	}
+	os.Args = args
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	cli, err := NewCLI()
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), "data.csv", cli.path)
+	assert.Equal(suite.T(), EXPLICIT, cli.mode)
 	assert.Equal(suite.T(), suite.orderedArgs, cli.args)
 }
 
