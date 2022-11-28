51723a5cbfaa5444fc264b5da3efeac801ae675b
chau-t-tran <chau.t.tran.us@gmail.com>
2022-11-28 02:16:07 -0500

    Regex filtering implementation

--- csv.go
+++ csv.go
@@ -4,7 +4,14 @@ import (
 	"bufio"
 	"errors"
 	"os"
+	"regexp"
 	"strings"
+)
+
+var (
+	ErrEOF                error = errors.New("EOF")
+	ErrFilterIncompatible error = errors.New("FilterIncompatible")
+	ErrFilterMismatch     error = errors.New("FilterMismatch")
 )
 
 type CSVHandler struct {
@@ -27,12 +34,33 @@ func (c *CSVHandler) NextRow() (row []string, err error) {
 	if !c.scanner.Scan() {
 		err = c.scanner.Err()
 		if err == nil {
-			err = errors.New("EOF")
+			err = ErrEOF
 		}
 		return
 	}
 
 	text := c.scanner.Text()
 	row = strings.Split(text, ",")
+	return
+}
+
+func (c *CSVHandler) NextRowWithFilter(filter []string) (row []string, err error) {
+	row, err = c.NextRow()
+	if err != nil {
+		return
+	}
+
+	if len(filter) != len(row) {
+		err = ErrFilterMismatch
+		return
+	}
+
+	for i, arg := range filter {
+		match, _ := regexp.MatchString(arg, row[i])
+		if !match {
+			err = ErrFilterIncompatible
+			return
+		}
+	}
 	return
 }
